310e4160109d496d92278863f3a6e6340d2776d4
voon <176486102+Vooncoding@users.noreply.github.com.>
2024-10-10 21:36:07 +0200

    hello

---
+++ script.js
@@ -0,0 +1,24 @@
+function getWeather() {
+    const apiKey = 'fa12bcbdf4c2a43d0a9660f05c0ab4ae';
+    const city = document.getElementById('city').value;
+
+    if (!city) {
+        alert('Please enter a city');
+        return;
+    }
+
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+}
+
+function getWeather() {
+    fetch(currentWeatherUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.error('Error fetching current weather data', error);
+            alert('Error fetchin weather data. please try again.'); 
+        });
+}
